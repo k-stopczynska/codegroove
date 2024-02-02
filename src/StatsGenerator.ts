979c8d1713c3cfd188ebde098db6b374b8f97300
k.stopczynska <k.stopczynska@gmail.com>
2024-02-02 11:40:41 +0100

    generating dynamic chart canvases and getting proper data for statistics charts

--- src/StatsGenerator.ts
+++ src/StatsGenerator.ts
@@ -16,93 +16,10 @@ export class StatsGenerator {
 		this.context = context;
 	}
 
-	init() {
-		this.panel.webview.html = this.getWebviewContent();
-		this.fetchData();
-	}
-
-	getWebviewContent() {
-		const logoPath = vscode.Uri.joinPath(
-			this.context.extensionUri,
-			'assets',
-			'codegroove.png',
-		);
-		const logoSrc = this.panel.webview.asWebviewUri(logoPath);
-
-		const stylePath = vscode.Uri.joinPath(
-			this.context.extensionUri,
-			'src',
-			'styles.css',
-		);
-		const styleSrc = this.panel.webview.asWebviewUri(stylePath);
-
-		const scriptPath = vscode.Uri.joinPath(
-			this.context.extensionUri,
-			'src',
-			'charts.js',
-		);
-		const scriptSrc = this.panel.webview.asWebviewUri(scriptPath);
-
-		return `<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="${styleSrc}">
-                <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
-                <script src="${scriptSrc}" defer type="module"></script>
-
-                <title>Code Timer Stats</title>
-            </head>
-            <body>
-                <nav class="nav__container">
-                    <img src="${logoSrc}" width="100" />
-                    <h1>codegroove stats</h1>
-                </nav>
-                <main>
-                    <section class="section__container">
-                        <div class="chart">
-                            <canvas id="chart1"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart2"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart3"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart4"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart5"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart6"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart7"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart8"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart9"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart10"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart11"></canvas>
-                        </div>
-                             <div class="chart">
-                            <canvas id="chart12"></canvas>
-                        </div>
-                       
-    
-                    </section>
-                </main>
-            </body>
-            </html>`;
+	async init() {
+		const durations = await this.fetchData();
+		const chartsHtml = this.generateChartsHtml(durations);
+		this.panel.webview.html = chartsHtml;
 	}
 
 	async fetchData() {
@@ -116,14 +33,10 @@ export class StatsGenerator {
 			const contentString = Buffer.from(content).toString();
 			const jsonData = JSON.parse(contentString);
 			const sessions = this.filterDates(jsonData);
-
-			// const chartsHtml = this.generateChartsHtml(jsonData);
-			// this.panel.webview.html = chartsHtml;
-
-			const durations =
-				sessions.map((session) => this.getDurationPerProjectAndPerLanguage(session));
-
-			console.log(durations);
+			const durations = sessions.map((session) =>
+				this.getDurationPerProjectAndPerLanguage(session),
+			);
+			return durations;
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}
@@ -138,8 +51,8 @@ export class StatsGenerator {
 
 	filterDates(data: Session[]) {
 		const currTime = this.getCurrentTime();
-        const splitted = currTime.split('/');
-        
+		const splitted = currTime.split('/');
+
 		const dailySessions = data.filter(
 			(data) => data.start.split('/')[1] === splitted[1],
 		);
@@ -176,84 +89,81 @@ export class StatsGenerator {
 		return [durationPerProject, durationPerLanguage];
 	}
 
-	// 	generateChartsHtml(data: any) {
-	// 		const logoPath = vscode.Uri.joinPath(
-	// 			this.context.extensionUri,
-	// 			'assets',
-	// 			'codegroove.png',
-	// 		);
-	// 		const logoSrc = this.panel.webview.asWebviewUri(logoPath);
+	generateChartsHtml(data: any) {
+		const logoPath = vscode.Uri.joinPath(
+			this.context.extensionUri,
+			'assets',
+			'codegroove.png',
+		);
+		const logoSrc = this.panel.webview.asWebviewUri(logoPath);
 
-	// 		const stylePath = vscode.Uri.joinPath(
-	// 			this.context.extensionUri,
-	// 			'src',
-	// 			'styles.css',
-	// 		);
-	// 		const styleSrc = this.panel.webview.asWebviewUri(stylePath);
+		const stylePath = vscode.Uri.joinPath(
+			this.context.extensionUri,
+			'src',
+			'styles.css',
+		);
+		const styleSrc = this.panel.webview.asWebviewUri(stylePath);
 
-	// 		const chartScriptPath = vscode.Uri.joinPath(
-	// 			this.context.extensionUri,
-	// 			'src',
-	// 			'charts.js',
-	// 		);
-	// 		const chartScriptSrc = this.panel.webview.asWebviewUri(chartScriptPath);
+		// const chartScriptPath = vscode.Uri.joinPath(
+		// 	this.context.extensionUri,
+		// 	'src',
+		// 	'charts.js',
+		// );
+		// const chartScriptSrc = this.panel.webview.asWebviewUri(chartScriptPath);
 
-	// 		const chartContainers = Object.keys(data).map((language, index) => {
-	// 			const chartData = data[language]; // Assuming your data structure
-	// 			const chartConfig = this.getChartConfiguration(language, chartData);
+		const chartContainers = data.flat().map((stat: any, index: any) => {
+			const chartConfig = this.getChartConfiguration('bar', stat, index);
 
-	// 			return `<div class="chart">
-	//               <canvas id="chart${index + 1}"></canvas>
-	//               <script>
-	//                 ${chartConfig}
-	//               </script>
-	//             </div>`;
-	// 		});
+			return `<div class="chart">
+	              <canvas id="chart${index + 1}"></canvas>
+	              <script defer>
+                  ${chartConfig}
+	              </script>
+	            </div>`;
+		});
 
-	// 		return `
-	//     <!DOCTYPE html>
-	//     <html lang="en">
-	//     <head>
-	//         <meta charset="UTF-8">
-	//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	//         <link rel="stylesheet" href="${styleSrc}">
-	//         <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
-	//         <script src="${chartScriptSrc}" defer type="module"></script>
-	//         <title>Code Timer Stats</title>
-	//     </head>
-	//     <body>
-	//             <nav class="nav__container">
-	//                 <img src="${logoSrc}" width="100" />
-	//                 <h1>codegroove stats</h1>
-	//             </nav>
-	//         <main>
-	//             <section class="section__container">
-	//                 ${chartContainers.join('')}
-	//             </section>
-	//         </main>
-	//     </body>
-	//     </html>`;
-	// 	}
+		return `
+	    <!DOCTYPE html>
+	    <html lang="en">
+	    <head>
+	        <meta charset="UTF-8">
+	        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	        <link rel="stylesheet" href="${styleSrc}">
+	        <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
+	       
+	        <title>Code Timer Stats</title>
+	    </head>
+	    <body>
+	            <nav class="nav__container">
+	                <img src="${logoSrc}" width="100" />
+	                <h1>codegroove stats</h1>
+	            </nav>
+	        <main>
+	            <section class="section__container">
+	                ${chartContainers.join('')}
+	            </section>
+	        </main>
+	    </body>
+	    </html>`;
+	}
 
-	// 	getChartConfiguration(language: string, data: any) {
-	// 		// Your logic to generate chart configuration based on data
-	// 		return `
-	//     const data_${language} = ${JSON.stringify(data)};
-	//     new Chart(
-	//       document.getElementById('chart${index + 1}'),
-	//       {
-	//         type: 'bar',
-	//         data: {
-	//           labels: data_${language}.map(row => row.year),
-	//           datasets: [
-	//             {
-	//               label: 'Acquisitions by year',
-	//               data: data_${language}.map(row => row.count),
-	//             },
-	//           ],
-	//         },
-	//       }
-	//     );
-	//   `;
-	// 	}
+	getChartConfiguration(type: string, data: any, index: any) {
+		return `
+	    new Chart(
+	      document.getElementById('chart${index + 1}'),
+	      {
+	        type: ${type},
+	        data: {
+	          labels: ${Object.keys(data)}.map(row => row),
+	          datasets: [
+	            {
+	              label: 'Acquisitions by year',
+	              data: ${Object.values(data)}.map(row => row),
+	            },
+	          ],
+	        },
+	      }
+	    );
+        `;
+	}
 }
