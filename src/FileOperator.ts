68df0b9c9c3733d16c719d412c0fb9e84db77d3f
k.stopczynska <k.stopczynska@gmail.com>
2024-02-23 11:08:12 +0100

    File Operator documentation

--- src/FileOperator.ts
+++ src/FileOperator.ts
@@ -13,9 +13,19 @@ export class FileOperator implements FileOperatorInstance {
 		this.readStats();
 	}
 
+	/**
+	 * uses vscode context to find the global storage location
+	 * @returns global storage Uri
+	 */
+
 	private getExtensionStoragePath() {
 		return this.context.globalStorageUri;
 	}
+
+	/**
+	 * joins global storage path with stats.csv file path
+	 * @returns stats.csv file path
+	 */
 
 	private getCsvFilePath() {
 		const extensionStoragePath = this.getExtensionStoragePath();
@@ -26,6 +36,12 @@ export class FileOperator implements FileOperatorInstance {
 		return csvFilePath;
 	}
 
+	/**
+	 * interacts with workspace local files and based on metadata found recognizes if file exists
+	 * @param fileUri file path to searched file
+	 * @returns promise resolved to true if file exists in provided Uri, else false
+	 */
+
 	private async isFileExists(fileUri: vscode.Uri): Promise<boolean> {
 		try {
 			await vscode.workspace.fs.stat(fileUri);
@@ -34,6 +50,11 @@ export class FileOperator implements FileOperatorInstance {
 			return false;
 		}
 	}
+
+	/**
+	 * creates stats.csv file with provided headings if file hasn't already been created
+	 * @returns early if file already exists
+	 */
 
 	private async createStatsFileWithHeadings() {
 		const csvFilePath = this.getCsvFilePath();
@@ -57,6 +78,11 @@ export class FileOperator implements FileOperatorInstance {
 		}
 	}
 
+	/**
+	 * creates readable stream from csv file and parses data into array of objects
+	 * @returns promise resolved to an array of objects or rejection if error occurs
+	 */
+
 	public async readStats() {
 		const csvData: Session[] = [];
 		const csvFilePath = this.getCsvFilePath();
@@ -79,16 +105,35 @@ export class FileOperator implements FileOperatorInstance {
 		});
 	}
 
+	/**
+	 * replaces null values with empty strings
+	 * @param _key key of the parsed object
+	 * @param value value of the parsed object
+	 * @returns value if it's not null, otherwise empty string
+	 */
+
 	private nullToEmptyReplacer(_key: string, value: any) {
 		return null === value ? '' : value;
 	}
 
-	private prepareDataItem(dataItem: Record<string, any>) {
+	/**
+	 * maps dataItem values with appropriate headings, converting them into strings
+	 * @param dataItem session data item to be mapped
+	 * @returns JSON string value if it's not null, otherwise empty string
+	 */
+
+	private prepareDataItem(dataItem: Record<string, Session>) {
 		return this.columns.map((column) => {
 			let value = dataItem[column] ?? '-';
 			return JSON.stringify(value, this.nullToEmptyReplacer);
 		});
 	}
+
+	/**
+	 * creates comma separated values separated by new lines
+	 * @param data array of objects containing session data
+	 * @returns strings of data with operating system end of line characters at the end
+	 */
 
 	private prepareCsvString(data: Record<string, any>[]) {
 		const contentRows = data.map((dataItem) => {
@@ -98,9 +143,14 @@ export class FileOperator implements FileOperatorInstance {
 		return csvDataString;
 	}
 
-	public async saveStats(updatedData: Session[]) {
+	/**
+	 * appends csv data string to existing file with operating system end of line characters at the end
+	 * @param newData sessions array to save to file
+	 */
+
+	public async saveStats(newData: Session[]) {
 		const csvFilePath = this.getCsvFilePath();
-		const content = this.prepareCsvString(updatedData) + os.EOL;
+		const content = this.prepareCsvString(newData) + os.EOL;
 
 		try {
 			await fs.promises.appendFile(csvFilePath.fsPath, content);
