4b1ab6e5a6520adb27db1e57fd88548ec49c1ce8
k.stopczynska <k.stopczynska@gmail.com>
2024-02-26 18:21:20 +0100

    added initialization test

--- src/test/codeTimer.test.ts
+++ src/test/codeTimer.test.ts
@@ -44,4 +44,22 @@ suite('CodeTimer Test Suite', () => {
 		expect(codeTimer.inactivityThreshold).to.equal(15 * 60 * 1000);
 		expect(codeTimer.inactivityTimer).to.be.null;
 	});
+
+	test('it should initialize with proper values', async () => {
+		codeTimer.init(fileOperatorMock);
+		expect(codeTimer.start).to.not.equal('');
+		expect(codeTimer.timer).to.not.be.undefined;
+		expect(codeTimer.id).to.not.equal('');
+		expect(codeTimer.project).to.not.equal('');
+		expect(codeTimer.lang).to.not.equal('');
+		expect(codeTimer.isSessionActive).to.be.true;
+		expect(codeTimer.duration).to.deep.equal({
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+		});
+		expect(codeTimer.sessions).to.deep.equal([]);
+		expect(codeTimer.inactivityThreshold).to.equal(15 * 60 * 1000);
+		expect(codeTimer.inactivityTimer).to.not.be.null;
+	});
 });
