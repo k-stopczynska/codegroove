415d636d125395c8958d73453e63026c65d729ab
k.stopczynska <k.stopczynska@gmail.com>
2024-03-06 12:33:48 +0100

    patch for styles and js paths

---
+++ static/charts.js
@@ -0,0 +1,26 @@
+/**
+ * defines and envokes initializing charts function that takes data from webview element, and creates Chart instance of provided type, labels and datasets
+ */
+
+(function initializeCharts() {
+  const data = JSON.parse(document.querySelector('.section__container').getAttribute('data'));
+  data.forEach((stat, index) => {
+    new Chart(
+      document.getElementById('chart' + (index + 1)),
+      {
+        type: stat.type,
+        data: {
+          labels: Object.keys(stat).filter((item) => item !== 'type'),
+          datasets: [
+            {
+              label: 'stats per hour',
+              data: Object.values(stat).filter((item) => !isNaN(item)),
+            },
+          ],
+        },
+      }
+    );
+  });
+})();
+
+
