5e6c811bd7b11eb1c8f945e54b4203a926a9f68e
k.stopczynska <k.stopczynska@gmail.com>
2024-03-18 19:19:08 +0100

    added listener for change events

--- src/Config.ts
+++ src/Config.ts
@@ -8,5 +8,30 @@ export class Config {
 			['5mins', '15mins', '30mins', '45mins', '1hour'],
 			{ placeHolder: 'Pick inactivity treshold' },
 		);
+		this.onChange();
+	}
+
+	onChange() {
+		this.inactivityDropdown.then((treshold: string) => {
+			let currentTreshold = 15;
+			switch (treshold) {
+				case '5mins':
+					currentTreshold = 5;
+					break;
+				case '30mins':
+					currentTreshold = 30;
+					break;
+				case '45mins':
+					currentTreshold = 45;
+					break;
+				case '1hour':
+					currentTreshold = 60;
+					break;
+				default:
+					break;
+			}
+			console.log('treshold', treshold);
+			console.log('currentTreshold', currentTreshold);
+		});
 	}
 }
