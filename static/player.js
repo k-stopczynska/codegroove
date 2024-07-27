62c21772dbcd1234558e02f432f736b7aa89865b
k.stopczynska <k.stopczynska@gmail.com>
2024-07-27 16:46:55 +0200

    hiding large play button when music is playing

--- static/player.js
+++ static/player.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     iframes.forEach((iframe) => {
         iframe.addEventListener('click', (event) => {
-            console.log('iframe clicked!')
             const videoUrl = iframe.getAttribute('src');
             vscode.postMessage({ command: 'openLink', videoUrl });
+
+            const playButton = iframe.querySelector('.ytp-large-play-button');
+            playButton.style.display = 'none';
         });
     });
 });
