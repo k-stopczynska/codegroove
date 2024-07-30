3031b32b2a39a7801e1204293d0c116a62d03719
k.stopczynska <k.stopczynska@gmail.com>
2024-07-30 20:40:37 +0200

    added toggling to buttons

--- static/player.js
+++ static/player.js
@@ -8,16 +8,30 @@ document.addEventListener('DOMContentLoaded', () => {
             vscode.postMessage({ command: 'openLink', videoUrl });
 
             const largePlayButton = iframe.querySelector('.ytp-large-play-button');
-            largePlayButton.style.display = 'none';
+            largePlayButton.classList.toggle('active');
 
             iframe.removeEventListener('click', handlePlayOnLoad);
 
             const playButton = iframe.querySelector('.ytp-play-button');
-            playButton.addEventListener('click', () => vscode.postMessage({ command: 'togglePlay'}));
+            const playIcon = playButton.querySelector('.play-icon');
+            const pauseIcon = playButton.querySelector('.pause-icon');
+
+            playButton.addEventListener('click', () => {
+                vscode.postMessage({ command: 'togglePlay' });
+                playIcon.classList.toggle('hidden');
+                pauseIcon.classList.toggle('hidden');
+                });
 
             const muteButton = iframe.querySelector('.ytp-mute-button');
-            muteButton.addEventListener('click', () => vscode.postMessage({ command: 'toggleMute'}));
-        }
+            const unmuteIcon = muteButton.querySelector('.unmute-icon');
+            const muteIcon = muteButton.querySelector('.mute-icon');
+
+            muteButton.addEventListener('click', () => {
+                vscode.postMessage({ command: 'toggleMute' });
+                unmuteIcon.classList.toggle('hidden');
+                muteIcon.classList.toggle('hidden');
+                });
+            }
         iframe.addEventListener('click', handlePlayOnLoad);
         });
     });
